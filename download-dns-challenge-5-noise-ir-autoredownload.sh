cea7cda92713e9baed0b98bc6fbcff3f54f41803
nanless <francis7999@outlook.com>
2024-05-27 16:09:25 +0800

    add auto redownload scripts

---
+++ download-dns-challenge-5-noise-ir-autoredownload.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/bash
+
+# ***** 5th DNS Challenge at ICASSP 2023*****
+# Noise data which is used in both tracks
+# Also download the impulse response data
+
+# All compressed noises files are ~39 GB
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# The directory structure of the unpacked data is:
+# +-- noise_fullband 
+
+BLOB_NAMES=(
+    noise_fullband/datasets_fullband.noise_fullband.audioset_000.tar.bz2
+    noise_fullband/datasets_fullband.noise_fullband.audioset_001.tar.bz2
+    noise_fullband/datasets_fullband.noise_fullband.audioset_002.tar.bz2
+    noise_fullband/datasets_fullband.noise_fullband.audioset_003.tar.bz2
+    noise_fullband/datasets_fullband.noise_fullband.audioset_004.tar.bz2
+    noise_fullband/datasets_fullband.noise_fullband.audioset_005.tar.bz2
+    noise_fullband/datasets_fullband.noise_fullband.audioset_006.tar.bz2
+
+    noise_fullband/datasets_fullband.noise_fullband.freesound_000.tar.bz2
+    noise_fullband/datasets_fullband.noise_fullband.freesound_001.tar.bz2
+
+    datasets_fullband.impulse_responses_000.tar.bz2
+)
+
+# BLOB_NAMES=(
+#     noise_fullband/datasets_fullband.noise_fullband.audioset_001.tar.bz2
+#     noise_fullband/datasets_fullband.noise_fullband.audioset_002.tar.bz2
+# )
+
+# BLOB_NAMES=(
+#     noise_fullband/datasets_fullband.noise_fullband.audioset_002.tar.bz2
+# )
+
+###############################################################
+
+AZURE_URL="https://dnschallengepublic.blob.core.windows.net/dns5archive/V5_training_dataset"
+
+OUTPUT_PATH="./datasets_fullband"
+
+mkdir -p $OUTPUT_PATH/noise_fullband
+
+for BLOB in ${BLOB_NAMES[@]}
+do
+    URL="$AZURE_URL/$BLOB"
+    echo "Download: $BLOB"
+
+    # DRY RUN: print HTTP response and Content-Length
+    # WITHOUT downloading the files
+    # curl -s -I "$URL" | head -n 2
+
+    # Actually download the files: UNCOMMENT when ready to download
+    # curl "$URL" -o "$OUTPUT_PATH/$BLOB"
+
+    # Same as above, but using wget
+    # wget "$URL" -O "$OUTPUT_PATH/$BLOB"
+
+    # Same, + unpack files on the fly
+    # curl "$URL" | tar -C "$OUTPUT_PATH" -f - -x -j
+
+    MAX_RETRIES=5
+    RETRY_DELAY=5
+    retry=0
+    while [ $retry -lt $MAX_RETRIES ]; do
+        curl "$URL" -o "$OUTPUT_PATH/$BLOB"
+
+        if [ $? -eq 0 ]; then
+            echo "下载成功"
+            break
+        else
+            echo "下载失败，尝试重新下载..."
+            retry=$((retry + 1))
+            sleep $RETRY_DELAY
+        fi
+    done
+
+    if [ $retry -eq $MAX_RETRIES ]; then
+        echo "达到最大重试次数，下载失败"
+    fi
+done
